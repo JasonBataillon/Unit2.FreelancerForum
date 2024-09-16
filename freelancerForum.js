b0c842a37e2c610eb157fe16417bde8d5f77c2f5
Your Name <jasonbataillon@gmail.com>
2024-09-16 18:32:12 -0400

    Created all the constants needed in the js doc and the general html template.

---
+++ freelancerForum.js
@@ -0,0 +1,15 @@
+const Names = ['Alice', 'John', 'Jill', 'Tommy', 'Ashe', 'Tim'];
+const Occupations = [
+  'Carpenter',
+  'Scientist',
+  'Janitor',
+  'Teacher',
+  'Astronaut',
+  'Librarian',
+];
+const Prices = ['$70', '$200', '$90', '$270', '$130', '$40'];
+
+const changeName = setInterval(blank, 10000);
+const changeOccupation = setInterval(blank, 10000);
+const changePrice = setInterval(blank, 10000);
+render();
